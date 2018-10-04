a16009e0b36119786c4a476c801d5b138d601b28
liceal <675024132@qq.com>
2018-10-04 11:09:50 +0800

    first commit

---
+++ Train1/Demo1.php
@@ -0,0 +1,5 @@
+<meta http-equiv=”Content-Type”content=”text/html;charset=utf-8”>
+<form method="post" action="Demo2.php">
+    姓名：<input type="text" name="username">
+    <input type="submit" value="提交">
+</form>
